f1823f290c09f7697a9da64c99a127f8551a2ec4
sasha <esmanchik@gmail.com>
2024-03-08 20:10:12 -0500

    Implemented domestic flight

--- Flights/FlightService.Tests/FlightServiceTest.cs
+++ Flights/FlightService.Tests/FlightServiceTest.cs
@@ -16,14 +16,37 @@ public class FlightServiceTest
     [Fact]
     public void ReadFile_WhenPathIsValid_ReturnsLegs()
     {
-        var path = Path.Combine("..", "..", "..", "iah-jfk.csv");
+        var path = GetIanJfkFilePath();
 
         var result = _flightService.ReadFile(path);
 
         Assert.Equal(3, result.Count);
         Assert.Equal("IAH", result[0].Origin);
+        var offset = TimeZoneInfo.Local.GetUtcOffset(DateTimeOffset.Now);
+        Assert.Equal(new DateTimeOffset(
+            2024, 4, 1, 
+            8, 0, 0, 0, 
+            result[0].Departure.Offset), result[0].Departure); 
         Assert.Equal("ORD", result[0].Destination);
         Assert.Equal("JFK", result[2].Origin);
         Assert.Equal("IAH", result[2].Destination);
     }
+
+    [Fact]
+    public void Journey_WhenDomesticFlight_FourHourLayoverSplitsSegments()
+    {
+        var path = Path.Combine("..", "..", "..", "iah-jfk.csv");
+        var legs = _flightService.ReadFile(path);
+        
+        var journey = _flightService.Journey(legs);
+
+        Assert.Equal(2, journey.Count);
+        Assert.Equal("IAH", journey[0].Origin);
+        Assert.Equal("JFK", journey[0].Destination);
+        Assert.Equal("JFK", journey[1].Origin);
+        Assert.Equal("IAH", journey[1].Destination);
+    }
+
+
+    private string GetIanJfkFilePath() => Path.Combine("..", "..", "..", "iah-jfk.csv");
 }
--- Flights/FlightService/FlightService.cs
+++ Flights/FlightService/FlightService.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using System.Globalization;
 
 namespace Flight.Services
 {
     public class FlightService
     {
+        public readonly HashSet<string> Domestic =  new(new string[] {"IAH", "ORD", "JFK", "DFW", "DSM"});
+        
+        public readonly HashSet<string> International = new(new string[] {"BOM", "DBX", "LHR"});
+
+        private const string TimePattern = "M-d-yyyy h:mm";
+        
         public bool IsSegment(int candidate)
         {
             return false;
@@ -20,13 +26,41 @@ namespace Flight.Services
                 (
                     Origin: cols[0],
                     Destination: cols[1],
-                    Departure: new DateTimeOffset(),
-                    Arrival: new DateTimeOffset()
+                    Departure: ParseTime(cols[2]),
+                    Arrival: ParseTime(cols[3])
                 );
                 result.Add(leg);
             }
             return result;
         }
 
+        public List<Leg> Journey(List<Leg> legs)
+        {
+            var from = 0;
+            var prev = 0;
+            var last = 1;
+            Leg segment;
+            var segments = new List<Leg>();
+            while (last < legs.Count)
+            {
+                var span = legs[last].Departure - legs[prev].Arrival;
+                Console.WriteLine($"{last} {legs[last].Destination} {span.TotalMinutes}");
+                if (span.TotalMinutes > 4 * 60) 
+                {
+                    segment = new Leg(legs[from].Origin, legs[prev].Destination, legs[from].Departure, legs[prev].Arrival);
+                    segments.Add(segment);
+                    from = last;
+                }
+                prev = last;
+                ++last;
+            }
+            segment = new Leg(legs[from].Origin, legs[prev].Destination, legs[from].Departure, legs[prev].Arrival);
+            segments.Add(segment);
+            //if (last == legs.Count) throw new Exception("Segment never ends");
+            return segments;
+        }
+
+        private DateTimeOffset ParseTime(string time) => DateTimeOffset.Parse(time);
+            //DateTimeOffset.ParseExact(time, TimePattern, CultureInfo.InvariantCulture);
     }
 }
